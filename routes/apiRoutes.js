b4e92dfffcfb2088f44ffebf0d95fb9af0982d30
Carlicia Smith <smith.carlicia@gmail.com>
2021-01-09 12:14:35 -0500

    commented out git rid of brackets on line 41 in apiRoutes file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -38,7 +38,7 @@ router.put('/api/workouts/:id', ({body, params}, res) => {
 // create workout
 
 router.post('/api/workouts', (req, res) => {
-    Workout.create({})
+    Workout.create()
     .then(createdWorkouts => res.json(createdWorkouts))
     .catch(err => {
         console.log("err", err)
